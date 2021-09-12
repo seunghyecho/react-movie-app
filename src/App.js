7b0ffc1638600488015935720dda5daea800c00e
seunghyecho <tmdgp523@naver.com>
2021-09-12 20:47:06 +0900

    #2.4 propTypes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import "./App.css";
 import React from "react";
+import PropTypes from "prop-types";
 
-function Test({ name, picture }) {
+function Test({ name, picture, rating }) {
   return (
     <div>
       <h1>it is {name}</h1>
+      <h3>{rating}/5.0</h3>
       <img src={picture} alt={name} />
     </div>
   );
 }
 
+Test.prototypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+};
+
 const foodILike = [
   {
+    id: "1",
     name: "one",
     Image:
       "https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg",
+    rating: 3,
   },
   {
+    id: "2",
     name: "two",
     Image: "https://t1.daumcdn.net/cfile/tistory/253FA23E57CD8A1915",
+    rating: 2,
   },
 ];
 
@@ -26,7 +38,12 @@ function App() {
   return (
     <div>
       {foodILike.map((current) => (
-        <Test name={current.name} picture={current.Image} />
+        <Test
+          key={current.id}
+          name={current.name}
+          picture={current.Image}
+          rating={current.rating}
+        />
       ))}
     </div>
   );
