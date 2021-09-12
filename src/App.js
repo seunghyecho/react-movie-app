8bd0703a98fbeacf90c4dd2316d396818be3c51f
seunghyecho <tmdgp523@naver.com>
2021-09-12 20:26:34 +0900

    #2.3 map Recap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+
+function Test({ name, picture }) {
+  return (
+    <div>
+      <h1>it is {name}</h1>
+      <img src={picture} alt={name} />
+    </div>
+  );
+}
+
+const foodILike = [
+  {
+    name: "one",
+    Image:
+      "https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg",
+  },
+  {
+    name: "two",
+    Image: "https://t1.daumcdn.net/cfile/tistory/253FA23E57CD8A1915",
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React meme
-        </a>
-      </header>
+    <div>
+      {foodILike.map((current) => (
+        <Test name={current.name} picture={current.Image} />
+      ))}
     </div>
   );
 }
